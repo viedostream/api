ab2f52ef3b974cf411d47c256778e074e3416c10
viedostream <viedoteam@gmail.com>
2020-08-17 18:11:53 +0430

    lowercase username and email

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -13,6 +13,8 @@ export class UserService {
   private readonly stateManager: StateManager;
 
   public async create(email: string, username: string, password: string): Promise<IUserLoginResponse> {
+    username = username.toLowerCase();
+    email = email.toLowerCase();
     const userId: string = await this.dataSource.createUser(email, username, password);
     const token: string = await this.stateManager.create(userId, username);
 
@@ -20,6 +22,7 @@ export class UserService {
   }
 
   public async login(emailOrUsername: string, password: string): Promise<IUserLoginResponse> {
+    emailOrUsername = emailOrUsername.toLowerCase();
     const user: IUser = await this.dataSource.getUserLogin(emailOrUsername, password);
     await this.stateManager.removeAll(user.userId);
     const token: string = await this.stateManager.create(user.userId, user.userName);
